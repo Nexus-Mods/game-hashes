a3e114b14caef9afa625e741c5272eb30e658655
halgari <tbaldridge@gmail.com>
2024-12-04 11:19:50 -0700

    update remap file

--- scripts/rename_folders.py
+++ scripts/rename_folders.py
@@ -3,7 +3,7 @@ import os
 import shutil
 
 # Load game name to ID mapping
-with open('json/ids.json', 'r') as f:
+with open('json/ids.json', 'rb') as f:
     game_mappings = json.load(f)
 
 # Iterate over the directories inside 'json' folder
@@ -11,6 +11,6 @@ base_path = 'json'
 for game_name in os.listdir(base_path):
     full_path = os.path.join(base_path, game_name)
     if os.path.isdir(full_path) and game_name in game_mappings:
-        game_id = game_mappings[game_name]
+        game_id = str(game_mappings[game_name])
         new_full_path = os.path.join(base_path, game_id)
         shutil.move(full_path, new_full_path)
